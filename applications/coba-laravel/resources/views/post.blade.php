3bd15eb735fccbbeeb90688ebcfdf93fdff01b93
danumardiko <danumardiko123@gmail.com>
2021-10-25 12:09:04 +0700

    menyelesaikan vidio 11. N+1 Problem
    menyelesaikan vidio 11. N+1 Problem

--- applications/coba-laravel/resources/views/post.blade.php
+++ applications/coba-laravel/resources/views/post.blade.php
@@ -7,11 +7,11 @@
 @section('container')
 <article>
 <h2 class="mb-5">{{ $post->title }}</h2>
-<p> By. <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in 
+<p> By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in 
 <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{$post->category->name}}</a></p>
 
 
-<h5>{{ $post["author"] }}</h5>
+
 {!! $post->body !!} 
 </article>
     <a href="/blog" class="d-block mt-3">Back to Posts</a>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e8f5632a57012325ae0ecf06f123f49f1102fdd5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e8f5632a57012325ae0ecf06f123f49f1102fdd5',
+            'dev_requirement' => false,
+        ),
+        'itsgoingd/clockwork' => array(
+            'pretty_version' => 'v5.1.0',
+            'version' => '5.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../itsgoingd/clockwork',
+            'aliases' => array(),
+            'reference' => 'b963dee47429a49c9669981cfa9a8362ce209278',
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'dev_requirement' => false,
+        ),
+    ),
+);
